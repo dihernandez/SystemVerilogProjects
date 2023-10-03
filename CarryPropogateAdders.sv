864b0213e72151b795c1710815d3f206655a67af
dihernandez <diher@mit.edu>
2023-10-03 19:29:44 -0400

    added carry lookahead adder

--- CarryPropogateAdders.sv
+++ CarryPropogateAdders.sv
@@ -18,6 +18,37 @@ module RippleCarryAdder #(parameter ADDR_WIDTH = 16) (
   assign c_out = carry[ADDR_WIDTH];
 endmodule
 
+module CarryLookaheadAdder #(parameter ADDR_WIDTH = 16) (
+  input logic[ADDR_WIDTH - 1:0] a, b,
+  input logic c_in,
+  output logic[ADDR_WIDTH - 1:0] sum,
+  output logic c_out
+); 
+
+  wire[ADDR_WIDTH:0] carry;
+  wire [ADDR_WIDTH - 1:0] gen, propogate;
+  
+  assign carry[0] = c_in;
+  
+  genvar it;
+  generate
+    for (it = 0; it < ADDR_WIDTH; it = it + 1) begin
+      FullAdder fa(a[it], b[it], carry[it], sum[it]);
+    end
+  endgenerate
+  
+  genvar ii;
+  generate
+    for (ii = 0; ii < ADDR_WIDTH; ii = ii + 1) begin
+      assign gen[ii] = a[ii] & b[ii];
+      assign propogate = a[ii] | b[ii];
+      assign carry[ii + 1] = gen[ii] | (propogate[ii] & carry[ii]);
+    end
+  endgenerate
+  
+  assign c_out = carry[ADDR_WIDTH];
+endmodule
+
 module FullAdder(
   input logic a, b, c_in,
   output logic sum, c_out);
--- CarryPropogateAddersTestbench.sv
+++ CarryPropogateAddersTestbench.sv
@@ -1,15 +1,15 @@
 module CPA_testbench();
-  
   logic clk;
-  logic[3:0] a, b, sum;
-  logic c_in, c_out;
+  logic[3:0] a, b, rca_sum, cla_sum;
+  logic c_in, rca_c_out, cla_c_out;
   
   always begin
   	clk = 1; #5;
     clk = 0; #5;
   end
   
-  RippleCarryAdder #(4) dut_rca(a, b, c_in, sum, c_out);
+  RippleCarryAdder #(4) dut_rca(a, b, c_in, rca_sum, rca_c_out);
+  CarryLookaheadAdder #(4) dut_cla(a, b, c_in, cla_sum, cla_c_out);
   
   initial begin
   	// Add 4+5 no carry in
@@ -17,18 +17,18 @@ module CPA_testbench();
     a <= 4;
     b <= 5;
     #10;
-    $display("case 1, sum is %d, c_out is %b", sum, c_out);
+    $display("case 1, rca_sum is %d, cla_sum is %d, rca_c_out is %b, cla_c_out is %b", rca_sum, cla_sum, rca_c_out, cla_c_out);
     // Add 1 + 0 with carry in
     c_in <= 1;
     a <= 1;
     b <= 0;
     #10;
-    $display("case 2, sum is %d, c_out is %b", sum, c_out);
+    $display("case 2, rca_sum is %d, cla_sum is %d, rca_c_out is %b, cla_c_out is %b", rca_sum, cla_sum, rca_c_out, cla_c_out);
     // Add 1 to 15 with a carry in
     c_in <= 1;
     a <= 15;
     b <= 1;
     #10;
-    $display("case 1, sum is %d, c_out is %b", sum, c_out);
+    $display("case 3, rca_sum is %d, cla_sum is %d, rca_c_out is %b, cla_c_out is %b", rca_sum, cla_sum, rca_c_out, cla_c_out);
   end
 endmodule
